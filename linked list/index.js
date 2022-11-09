d888ded4e83db09b5ebd67192af193fa01615bbb
al-jabir <aljabir098@gmail.com>
2022-11-09 07:40:38 +0600

    linkedlist

--- linked list/index.js
+++ linked list/index.js
@@ -1,1 +1,30 @@
-console.log(1998);
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+  append(newNode) {
+    let node = this.head;
+    if (node === null) {
+      //empty linked list
+      this.head = newNode;
+      return this;
+    }
+    // while (node.next) {
+    //   node = node.next;
+    // }
+    node.next = newNode;
+  }
+}
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+//9, 19, 27
+let newLinkedList = new LinkedList();
+let newNode1 = new Node(9);
+console.log(newLinkedList.append(newNode1));
+console.log(newLinkedList.append(new Node(18)));
+console.log(newLinkedList.append(new Node(27)));
