a763687450442db662f69fff9e44522c0188cd5f
al-jabir <aljabir098@gmail.com>
2022-11-10 20:07:00 +0600

    LF

---
+++ LinkedList/linked.js
@@ -0,0 +1,17 @@
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+  // append data
+}
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+let myList = new LinkedList();
+
+console.log(myList);
