36a33a3f4364fcdec3e6915ab4ce975f0bef89cb
al-jabir <aljabir098@gmail.com>
2022-11-11 23:57:23 +0600

    check

--- Stack/app.js
+++ Stack/app.js
@@ -57,3 +57,31 @@ console.log(stack.size());
 
 stack.clear();
 console.log(stack.items);
+
+// check
+
+var Stack = function () {
+  this.top = null;
+  this.size = 0;
+};
+
+var Node = function (data) {
+  this.data = data;
+  this.previous = null;
+};
+
+Stack.prototype.push = function (data) {
+  var node = new Node(data);
+
+  node.previous = this.top;
+  this.top = node;
+  this.size += 1;
+  return this.top;
+};
+
+Stack.prototype.pop = function () {
+  temp = this.top;
+  this.top = this.top.previous;
+  this.size -= 1;
+  return temp;
+};
