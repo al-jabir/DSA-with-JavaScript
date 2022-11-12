6c1bfec5dd97756ba3abe99bf6312c21b8726586
al-jabir <aljabir098@gmail.com>
2022-11-12 21:21:53 +0600

    isShift

--- Queue/app.js
+++ Queue/app.js
@@ -6,6 +6,12 @@ class Queue {
   add(data) {
     return this.queue.push(data);
   }
+
+  // isShift
+
+  isShift() {
+    return this.queue.shift();
+  }
 }
 
 let newQueue = new Queue();
@@ -13,5 +19,7 @@ let newQueue = new Queue();
 newQueue.add('kign');
 newQueue.add('will be');
 newQueue.add('mafia');
+
+newQueue.isShift();
 
 console.log(newQueue);
