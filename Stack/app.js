04738737d41f91608f866bfda89568f389d484eb
al-jabir <aljabir098@gmail.com>
2022-11-12 19:43:28 +0600

    isPush

--- Stack/app.js
+++ Stack/app.js
@@ -6,8 +6,20 @@ class Stack {
   constructor() {
     this.StackItems = [];
   }
+
+  // isPush
+
+  isPush(item) {
+    this.StackItems.push(item);
+  }
 }
 
 let newStack = new Stack();
+
+newStack.isPush('fucking');
+newStack.isPush('teen fucked');
+newStack.isPush(25);
+newStack.isPush(19);
+newStack.isPush('load fucks');
 
 console.log(newStack);
