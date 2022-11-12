478fe14a1d551c511f9f41094e8539c903ca5141
al-jabir <aljabir098@gmail.com>
2022-11-12 19:57:56 +0600

    lastItem and isEmpty

--- Stack/app.js
+++ Stack/app.js
@@ -24,9 +24,26 @@ class Stack {
     }
     return this.StackItems.pop();
   }
+
+  // helpers
+  lastItem() {
+    return this.StackItems[this.StackItems.length - 1];
+  }
+
+  // isEmpty
+
+  isEmpty() {
+    if (this.StackItems.length === 0) {
+      return true;
+    }
+    return false;
+  }
 }
 
 let newStack = new Stack();
+// check isEmpty
+
+console.log(newStack.isEmpty());
 
 newStack.isPush('fucking');
 newStack.isPush('teen fucked');
@@ -34,10 +51,19 @@ newStack.isPush(25);
 newStack.isPush(19);
 newStack.isPush('load fucks');
 
+// check lastItem
+
+console.log(newStack.lastItem());
+
 // isPop
-newStack.isPop();
-newStack.isPop();
+
 newStack.isPop();
 newStack.isPop();
 
+// check isEmpty
+
+console.log(newStack.isEmpty());
+
+// lastItem
+newStack.lastItem();
 console.log(newStack);
