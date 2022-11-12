d48cbaa0c281b19c01e64c8798093451b8163f5f
al-jabir <aljabir098@gmail.com>
2022-11-12 21:29:50 +0600

    enqueue

--- Queue/app.js
+++ Queue/app.js
@@ -23,3 +23,36 @@ newQueue.add('mafia');
 newQueue.isShift();
 
 console.log(newQueue);
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+class QueueLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
+}
+
+let newQueues = new QueueLinkedList();
+
+newQueues.enqueue('Nafiul');
+console.log(newQueues);
