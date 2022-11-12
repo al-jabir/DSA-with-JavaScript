9ef80d83ed4211775c5d46d687b892ea1a30d123
al-jabir <aljabir098@gmail.com>
2022-11-12 10:06:10 +0600

    toString

--- Test/linked.js
+++ Test/linked.js
@@ -17,11 +17,30 @@ class Linked {
   isLength() {
     let node = this.head;
     let len = 0;
+    // loop check
     while (node) {
       len++;
       node = node.next;
     }
     return len;
+  }
+  toString() {
+    let node = this.head;
+    let str = '';
+    while (node) {
+      if (Array.isArray(node.data)) {
+        node.data = JSON.stringify(node.data);
+      }
+      if (node.next === null) {
+        str += `${node.data} `;
+        node = node.next;
+      } else {
+        str += `${node.data}, `;
+        node = node.next;
+      }
+    }
+
+    return str;
   }
 }
 
@@ -39,6 +58,8 @@ console.log(myNode);
 list.append(new Node('king'));
 list.append(new Node('Mafia'));
 list.append(new Node(1998));
+list.append(new Node(['fuck', 'fucked', { name: 'Emma', age: 22 }, 2003]));
 
 console.log(list);
 console.log(list.isLength());
+console.log(list.toString());
